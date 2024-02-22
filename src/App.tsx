2810745f19982ce047f283c9785e689883ef8418
saro1993 <116103507+saro1993@users.noreply.github.com>
2024-02-22 14:18:10 +0100

    first commit :)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export function App() {
+  return (
+    //Hoved comoonent for å kjøre hele programet!
+    //<GeoMap />
+    <>
+      <p> Welcome to our page! </p>
+    </>
+  );
+}
+
+

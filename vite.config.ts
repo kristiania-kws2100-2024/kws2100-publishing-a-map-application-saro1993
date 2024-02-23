0fdf1084554c53def5ef95c49b9c387bee323269
saro1993 <116103507+saro1993@users.noreply.github.com>
2024-02-23 10:29:57 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "kws2100-publishing-a-map-application-saro1993",
   plugins: [react()],
 });

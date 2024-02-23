50073974773bd46de049c6444f292810f813542b
saro1993 <116103507+saro1993@users.noreply.github.com>
2024-02-23 10:05:17 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 export default {
-  base: "https://github.com/kristiania-kws2100-2024/kws2100-publishing-a-map-application-saro1993.git/",
+  base: "kws2100-publishing-a-map-application-saro1993",
 };

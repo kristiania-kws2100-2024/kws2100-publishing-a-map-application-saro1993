959a87fdb301dab487ba3d8563043a5ed4af9197
saro1993 <116103507+saro1993@users.noreply.github.com>
2024-02-27 11:40:01 +0100

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { Application } from "../src/modules/application/application";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<Application />);

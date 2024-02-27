0a5e440ff4836d36f535c4209f0adfe6a17c498e
saro1993 <116103507+saro1993@users.noreply.github.com>
2024-02-27 12:31:04 +0100

    updated

--- src/modules/emergrncy-shelter/emergencyLayerCheckbox.tsx
+++ src/modules/emergrncy-shelter/emergencyLayerCheckbox.tsx
@@ -113,7 +113,7 @@ export function EmergencyLayerCheckbox() {
           checked={checked}
           onChange={(e) => setChecked(e.target.checked)}
         />
-        Show emergency shelterss
+        Show emergency sheltersss
         {activeFeature &&
           " (" +
             activeFeature.getProperties().adresse +
